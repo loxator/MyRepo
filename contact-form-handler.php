289baa8aec9842786c029664581469902c95ad7e
Amin Limbada <aminlimbada94@gmail.com>
2018-12-30 15:40:22 +0400

    # This is a combination of 13 commits. # The first commit's message is: Renamed index.html to index.php
    # The 2nd commit message will be skipped:
    
    #	Renamed index.html to index.php
    
    # The 3rd commit message will be skipped:
    
    #	deploying to heroku
    
    # The 4th commit message will be skipped:
    
    #	Deploying to heroku...again
    
    # The 5th commit message will be skipped:
    
    #	deploying to heroku yet again
    
    # The 6th commit message will be skipped:
    
    #	deploying to heroku yet again
    
    # The 7th commit message will be skipped:
    
    #	deploying to heroku yet again
    
    # The 8th commit message will be skipped:
    
    #	Change single quotes to double quotes for PHP
    
    # The 9th commit message will be skipped:
    
    #	Change single quotes to double quotes for PHP
    
    # The 10th commit message will be skipped:
    
    #	Change single quotes to double quotes for PHP
    
    # The 11th commit message will be skipped:
    
    #	added PHP Mailer
    
    # The 12th commit message will be skipped:
    
    #	added PHP Mailer
    
    # The 13th commit message will be skipped:
    
    #	Added lock file

---
+++ contact-form-handler.php
@@ -0,0 +1,40 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load composer's autoloader
+require 'vendor/autoload.php';
+
+
+$email = $_Post["email"];
+$message = $_Post["message"];
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'aminlimbada94@gmail.com';                 // SMTP username
+    $mail->Password = 'amddfygfcllqtrnh';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom($email, 'Mailer');
+    $mail->addAddress($to);     // Add a recipient
+    
+    $mail->Subject = 'Message from your personal website';
+    $mail->Body    = $message;
+    
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    header("Location: home.html");
+}
+?>
